aba2a8ef92349e56ef44576b3954d3c081a13b29
frandevelopez <frandevelopez@gmail.com>
2020-05-05 15:42:22 +0200

    added get_[object,list]_or_404

--- aplicacion/fichas/views.py
+++ aplicacion/fichas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,get_list_or_404,render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Client, Ficha
@@ -6,12 +6,12 @@ from .models import Client, Ficha
 # Create your views here.
 
 def index(request):	
-	latest_client_list = Client.objects.order_by('-creation_date')[:5]
+	latest_client_list = get_list_or_404(Client.objects.order_by('-creation_date'))[:5]
 	context = {'latest_client_list': latest_client_list,}
 	return render(request,'fichas/index.html',context)
 
 def client_detail(request, client_id):
-	client = Client.objects.get(pk=client_id)
-	fichas = Ficha.objects.filter(client=client_id)
+	client = get_object_or_404(Client, pk=client_id)
+	fichas = get_list_or_404(Ficha, client=client_id)
 	return render(request,'fichas/client_detail.html',{'client':client, 'fichas':fichas})
 
