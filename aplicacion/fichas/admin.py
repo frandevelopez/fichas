056ba2cfe7747c5bde232435efa5e30094cec585
frandevelopez <frandevelopez@gmail.com>
2020-05-05 13:08:18 +0200

    added models

--- aplicacion/fichas/admin.py
+++ aplicacion/fichas/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Client, Ficha
+from .models import Client, Ficha, Color, Supplier, Leather, Lining, Hardware
 
 
 admin.site.register(Client)
 admin.site.register(Ficha)
+admin.site.register(Color)
+admin.site.register(Supplier)
+admin.site.register(Leather)
+admin.site.register(Lining)
+admin.site.register(Hardware)
+
 
 # Register your models here.
--- aplicacion/fichas/models.py
+++ aplicacion/fichas/models.py
@@ -3,19 +3,63 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Ficha(models.Model):
-	style_name = models.CharField(max_length=200)
-	client = models.ForeignKey('Client',on_delete=models.CASCADE)
-	style_price=models.DecimalField(max_digits=5,default=0, decimal_places=2)
-
+class Supplier(models.Model):
+	name = models.CharField(max_length=200)
+	country = models.CharField(max_length=200)
 	def __str__(self):
-		return self.style_name
-
+		return self.name
 
 class Client(models.Model):
-	client_name = models.CharField(max_length=200)
-	client_country = models.CharField(max_length=200)
+	name = models.CharField(max_length=200)
+	country = models.CharField(max_length=200)
 	creation_date = models.DateTimeField('date published', default=timezone.now())
 
 	def __str__(self):
-		return self.client_name
+		return self.name
+
+class Ficha(models.Model):
+	name = models.CharField(max_length=200)
+	client = models.ForeignKey('Client',on_delete=models.CASCADE,default=None)
+	price = models.DecimalField(max_digits=5,default=0, decimal_places=2)
+	upper = models.ForeignKey('Leather',on_delete=models.CASCADE,default=None)
+	lining = models.ForeignKey('Lining',on_delete=models.CASCADE,default=None)
+	edge_apint = models.CharField(max_length=200,default=None)
+	thread = models.CharField(max_length=200,default=None)
+	sku = models.CharField(max_length=200)
+
+	creation_date = models.DateTimeField('date published', default=timezone.now())
+
+	def __str__(self):
+		return self.name
+
+class Color(models.Model):
+	name = models.CharField(max_length=200)
+	def __str__(self):
+		return self.name
+
+class Leather(models.Model):
+	name = models.CharField(max_length=200)
+	supplier = models.ForeignKey('Supplier',on_delete = models.CASCADE,default=None)
+	price = models.DecimalField(max_digits=5,default=0, decimal_places=2)
+	moq = models.DecimalField(max_digits=5,default=0, decimal_places=2)
+	def __str__(self):
+		return self.name
+
+class Hardware(models.Model):
+	name = models.CharField(max_length=200)
+	supplier = models.ForeignKey('Supplier',on_delete = models.CASCADE, default=None)
+	price = models.DecimalField(max_digits=5,default=0, decimal_places=2)
+	moq = models.DecimalField(max_digits=5,default=0, decimal_places=2)
+	def __str__(self):
+		return self.name
+
+class Lining(models.Model):
+	name = models.CharField(max_length=200)
+	supplier = models.ForeignKey('Supplier',on_delete = models.CASCADE,default=None)
+	price = models.DecimalField(max_digits=5,default=0, decimal_places=2)
+	roll_height = models.DecimalField(max_digits=5,default=0, decimal_places=2)
+	moq = models.DecimalField(max_digits=5,default=0, decimal_places=2)
+	def __str__(self):
+		return self.name
+
+
