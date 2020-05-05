6f0e053520783cb33d4b916c4c37e4a4db7834d8
frandevelopez <frandevelopez@gmail.com>
2020-05-05 15:49:22 +0200

    url namespace fichas

--- aplicacion/fichas/urls.py
+++ aplicacion/fichas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'fichas'
 urlpatterns = [
 	path('',views.index),
 	path('<int:client_id>/', views.client_detail, name='client_detail'),
